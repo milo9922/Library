d479abcce0a4ce49dd39c84ff84b3df3a2c733e5
milo9922 <52604121+milo9922@users.noreply.github.com>
2020-05-19 19:31:10 +0200

    create sql query which fill TB_BOOK with real books examples

---
+++ src/main/sql/fillUsingExamples.sql
@@ -0,0 +1,15 @@
+INSERT INTO TB_BOOK (BookID, Title, Author, PagesNum, AddedBy, AddDate)
+VALUES (1, 'Przełęcz Diatłowa. Tajemnica dziewięciorga', 'Anna Matwiejewa', 304, 3, CURRENT_DATE),
+       (2, 'Dylemat', 'Paris B.A.', 352, 3, CURRENT_DATE),
+       (3, 'Czysty kod. Podręcznik dobrego programisty', 'Robert C. Martin', 424, 3, CURRENT_DATE),
+       (4, 'Unity. Przewodnik projektanta gier', 'Geig Mike', 464, 3, CURRENT_DATE),
+       (5, 'Unorthodox. Jak porzuciłam świat ortodoksyjnych Żydów', 'Deborah Feldman', 272, 3, CURRENT_DATE),
+       (6, 'Jak naprawić sprzęt elektroniczny. Poradnik dla nieelektronika. Wydanie II', 'Michael Jay Geier', 416, 3,
+        CURRENT_DATE),
+       (7, 'Spring w akcji. Wydanie V', 'Craig Walls', 520, 3, CURRENT_DATE),
+       (8, 'Włam się do mózgu', 'Radosław Kotarski', 300, 3, CURRENT_DATE),
+       (9, 'Gdyby nie ty', 'Colleen Hoover', 400, 3, CURRENT_DATE),
+       (10, 'Była sobie rzeka', 'Diane Setterfield', 480, 3, CURRENT_DATE),
+       (11, 'Pacjentka', 'Alex Michaelides', 352, 3, CURRENT_DATE),
+       (12, 'Nowy wspaniały świat', 'Aldous Huxley', 256, 3, CURRENT_DATE),
+       (13, 'Gdzie śpiewają raki', 'Delia Owens', 416, 3, CURRENT_DATE);
