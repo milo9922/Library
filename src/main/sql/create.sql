8fe98a5c658317a45d9d82c3ebce2a4e16014c18
milo9922 <52604121+milo9922@users.noreply.github.com>
2020-05-24 17:51:19 +0200

    change title and author max length add blob borrowQrCode column

--- src/main/sql/create.sql
+++ src/main/sql/create.sql
@@ -9,14 +9,15 @@ CREATE TABLE IF NOT EXISTS TB_USER
 
 CREATE TABLE IF NOT EXISTS TB_BOOK
 (
-    BookID          INT UNIQUE  NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    Title           VARCHAR(30) NOT NULL,
-    Author          VARCHAR(30) NOT NULL,
+    BookID          INT UNIQUE   NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    Title           VARCHAR(100) NOT NULL,
+    Author          VARCHAR(50)  NOT NULL,
     PagesNum        INT,
     AddedBy         INT,
     AddDate         DATE,
     BorrowedBy      INT,
     BorrowDate      DATE,
+    BorrowQrCode    BLOB,
     ReturnDate      DATE,
     NumberOfBorrows INT
 );
