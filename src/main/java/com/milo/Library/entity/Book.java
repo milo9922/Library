cb4291dc11043bb647c055ca77c2bfbe1bb3b4ce
milo9922 <52604121+milo9922@users.noreply.github.com>
2020-05-31 16:17:33 +0200

    add entity, table and validation annotations add borrowQrCode blob field

--- src/main/java/com/milo/Library/entity/Book.java
+++ src/main/java/com/milo/Library/entity/Book.java
@@ -2,21 +2,35 @@ package com.milo.Library.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.sql.Blob;
 import java.util.Date;
 
+@Entity
+@Table(name = "TB_BOOK")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Book {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int bookId;
+    @NotEmpty
     private String title;
+    @NotEmpty
     private String author;
     private int pagesNum;
     private int addedBy;
+    @NotNull
     private Date addDate;
     private int borrowedBy;
     private Date borrowDate;
+    private Blob borrowQrCode;
     private Date returnDate;
     private int numberOfBorrows;
 
